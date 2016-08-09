c915ba1d8e84e5c2f33817b767506fa83c99f628
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-08-09 19:06:16 -0400

    add coupon banner custom

---
+++ assets/themes/goodcover/lib/inc/couponbanner.php
@@ -0,0 +1,37 @@
+<?php
+add_action('genesis_before_footer','msdlab_show_coupon_banner',4);
+
+function msdlab_show_coupon_banner(){
+    $fp = get_option('page_on_front');
+    $template = get_post_meta($fp,'_wp_page_template',TRUE);
+    if($template == 'page-sectioned.php'){
+        global $msd_custom_pages,$sectioned_page_metabox;
+        $i = 0;
+        $meta = $sectioned_page_metabox->the_meta($fp);
+        if(is_object($sectioned_page_metabox)){
+        while($sectioned_page_metabox->have_fields('sections')){
+            $layout = $sectioned_page_metabox->get_the_value('layout');
+            $name = $sectioned_page_metabox->get_the_value('section-name');
+            if($name != 'coupon'){continue;}
+            switch($layout){
+                case "four-col":
+                    $sections[] = MSDSectionedPage::column_output($meta['sections'][$i],$i);
+                    break;
+                case "three-col":
+                    $sections[] = MSDSectionedPage::column_output($meta['sections'][$i],$i);
+                    break;
+                case "two-col":
+                    $sections[] = MSDSectionedPage::column_output($meta['sections'][$i],$i);
+                    break;
+                default:
+                    $sections[] = MSDSectionedPage::default_output($meta['sections'][$i],$i);
+                    break;
+            }
+            $i++;
+        }//close while
+        print '<div class="sectioned-page-wrapper">';
+        print implode("\n",$sections);
+        print '</div>';
+        }//clsoe if
+    }
+}
